23c401384b65c44f3b2ce7e4375a3ccf16501430
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2023-04-08 13:31:45 +0300

    JAVA QA L3 z2

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate(double heightMeter, double weightKilogramm){
+        double result=weightKilogramm/(heightMeter*heightMeter);
+        int index= (int) result;
+        return index;
+    }
+}
